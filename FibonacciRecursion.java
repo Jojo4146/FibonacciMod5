4f9dc022a4d1e26d75a51c2e6198a2912b13a377
Johanna Scharbaai-Emilien <jse4146@me.com>
2023-02-23 18:01:41 -0500

    Computing time of implementation

--- FibonacciRecursion.java
+++ FibonacciRecursion.java
@@ -2,18 +2,13 @@ public class FibonacciRecursion {
     // Start of Fibonacci sequence with recursion
     static int numA = 0;
     static int numB = 1;
-
-    static void printFb(int n2){
-
+    static void printFb(int n2){  // Parameter is the number of iterations (15)
         if (n2 > 0) {
             int numC = numA + numB;
             numA = numB;
             numB = numC;
             System.out.print(" " + numC);
-            printFb(n2 - 1);
+            printFb(n2 - 1);   // Recursion
         }
-
-
     }
-
 }
--- Main.java
+++ Main.java
@@ -1,11 +1,10 @@
 public class Main {
     public static void main(String[] args){
-        // Variables
+        // Variables for first Fibonacci implementation
         int num1 = 0;
         int num2 = 1;
         int num3;
-        int i;
-        int n = 15;
+        int n = 15;  // # of iterations
 
         // Start of table creation
         System.out.println("\t\tImplementing Fibonacci function in recursive & iterative fashion");
@@ -15,7 +14,7 @@ public class Main {
 
         // Start of calculations of non-recursive calcs for Fibonacci sequence
         System.out.print(num1 + " " + num2);  // Prints first two numbers
-        for(i = 2; i < n; ++i){
+        for(int i = 2; i < n; ++i){  // For loop that iterates for n times
             num3 = num1 + num2;
             System.out.print(" " + num3);
             num1 = num2;
@@ -31,14 +30,15 @@ public class Main {
         System.out.print("Recursive      |  ");
 
         // Fibonacci sequence with recursion
-        long startTime2 = 0;
-        int n2 = 15;
+        long startTime2 = 0;  // Start time for recursion
+        // Variables for Fibonacci sequence in recursion
+        int n2 = 15; // # of iterations
         int numA = 0;
         int numB = 1;
         System.out.print(numA + " " + numB);  // printing 0 and 1 variables
         FibonacciRecursion.printFb(n2 - 2); // omitting last two numbers
         
-        long duration = System.nanoTime() - startTime2;
+        long duration = System.nanoTime() - startTime2;  // duration of recursion
         System.out.println(" | " + duration + "-ns");
     }
 }
